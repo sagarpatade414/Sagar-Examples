22a401dce9d07c40b162f53c01e1afb8080d120b
Sagar Patade <sagarpatade414@gmail.com>
2019-10-01 14:17:56 -0400

    Anagram solution
    Anagram solution

---
+++ src/Anagram.java
@@ -0,0 +1,34 @@
+
+public class Anagram {
+	
+	public static void main(String[] args) {
+		String s = "anagram", t = "nagaram";
+		isAnagram(s, t);
+	}
+	
+	
+	
+	public static boolean isAnagram(String s, String t) {
+		
+		if(s.length() != t.length()) {
+			return false;
+		}
+		
+		int[] count = new int[26];
+		
+		for(int i=0; i<s.length(); i++) {
+			count[s.charAt(i) - 'a']++;
+			count[t.charAt(i) - 'a']--;
+		}
+		
+		for(int c : count) {
+			if(c != 0)  {
+				return false;
+			}
+		}
+		
+		return true;   
+    }
+
+	
+}
