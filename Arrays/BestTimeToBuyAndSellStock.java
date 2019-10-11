a62b7ffe901e65d81beb05303be410ab20e68218
Sagar Patade <sagarpatade414@gmail.com>
2019-10-10 20:33:21 -0400

    findtheCelebrity and BestTimeToBuyStock
    findtheCelebrity and BestTimeToBuyStock

--- Arrays/BestTimeToBuyAndSellStock.java
+++ Arrays/BestTimeToBuyAndSellStock.java
@@ -2,10 +2,10 @@ public class BestTimeToBuyAndSellStock {
 	
 	public static void main(String[] args) {
 		int[] prices = {7,1,5,3,6,4};
+		
 		int maxProfit = maxProfit(prices);
 		
 		System.out.println("maxProfit : " + maxProfit);
-		
 	}
 	
 	
