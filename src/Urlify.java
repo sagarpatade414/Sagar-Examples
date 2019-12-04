531372df75949edf5130f76848f7cd8c81ce6ae5
Sagar Patade <sagarpatade414@gmail.com>
2019-12-04 12:18:14 -0500

    LinkedList
    LinkedList

---
+++ src/Urlify.java
@@ -0,0 +1,13 @@
+
+public class Urlify {
+	
+	
+	public static void main(String[] args) {
+		
+		String str="Mr Sagar Patade   ";  
+		System.out.println(str.length());
+		
+		char[] charArray = str.toCharArray();
+		System.out.println(charArray.length);
+	}
+}
