6aa6ec8abf0cc9979e99b0387ace47066417fb6b
Sagar Patade <sagarpatade414@gmail.com>
2019-12-15 13:11:57 -0500

    FindFIrstNonRepeatingInteger
    FindFIrstNonRepeatingInteger

---
+++ src/Random/FindFirstNonRepeating.java
@@ -0,0 +1,48 @@
+package Random;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class FindFirstNonRepeating {
+  
+	public static void main(String[] args) {
+		int[] nums = {9,2,3,2,6,6};
+		int res = findFirstNonRepeatingInt(nums);
+		
+		
+		System.out.println(res);
+		
+	}
+	
+	public static int findFirstNonRepeatingInt(int[] nums) {
+		int result = -1;
+		
+		if(nums == null || nums.length == 0) {
+			return result;
+		}
+		
+		Map<Integer, Integer> hmap = new LinkedHashMap<>();
+		
+		for(int i=0; i<nums.length; i++){
+			if(hmap.containsKey(nums[i])) {
+				hmap.remove(nums[i]);
+			} else {
+				hmap.put(nums[i], nums[i]);
+			} 
+		}
+		
+		Set<Integer> keySet = hmap.keySet();
+		
+		for(Integer k : keySet) {
+			return k;
+		}
+		
+		
+		
+		
+		return result;
+	}
+}
