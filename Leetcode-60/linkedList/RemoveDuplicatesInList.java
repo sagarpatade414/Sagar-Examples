4aa4168a1edbe78ccdd73733e2d6326251225f2e
Sagar Patade <sagarpatade414@gmail.com>
2019-12-28 13:05:12 -0500

    Linked List II
    Linked List II

---
+++ Leetcode-60/linkedList/RemoveDuplicatesInList.java
@@ -0,0 +1,19 @@
+package linkedList;
+
+public class RemoveDuplicatesInList {
+	
+	public class LisNode{
+		int data;
+		LisNode next;
+		
+		public LisNode(int data){
+			this.data = data;
+			this.next = null;
+		}
+		
+	}
+	
+	public static void main() {
+		
+	}
+}
