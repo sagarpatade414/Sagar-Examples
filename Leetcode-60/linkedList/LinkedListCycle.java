e745d4b66e10804eec07959976d54908942b09b7
Sagar Patade <sagarpatade414@gmail.com>
2019-12-28 11:50:42 -0500

    LinkedList Cycle I
    LinkedList Cycle I

---
+++ Leetcode-60/linkedList/LinkedListCycle.java
@@ -0,0 +1,40 @@
+package linkedList;
+
+public class LinkedListCycle {
+	
+	class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode(int x) {
+		          val = x;
+		          next = null;
+	     }
+	 }
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static boolean hasCycle(ListNode head) {
+		
+		if(head == null){
+			return false;
+		}
+		
+		ListNode slow = head;
+		ListNode fast = head.next;
+		
+		while(slow != fast) {
+			
+			if(fast == null || fast.next == null) {
+				return false;
+			}
+			
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		
+		
+        return true;
+    }
+}
