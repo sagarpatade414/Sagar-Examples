87d7324445b1fd3d504f4581e24c77d5133e4a89
Sagar Patade <sagarpatade414@gmail.com>
2019-12-29 19:55:00 -0500

    Path Sum BST
    Path Sum BST

---
+++ Leetcode-60/Trees/PathSum.java
@@ -0,0 +1,23 @@
+package Trees;
+
+
+public class PathSum {
+	
+	public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	 }
+	
+	public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null){
+            return false;
+        } else if(root.left == null && root.right == null && sum - root.val == 0){
+            return true;
+        } else {
+            return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        }
+        
+    }
+}
