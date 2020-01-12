7e3099bf522cacd08f800aace4c0535b49cd510a
Sagar Patade <sagarpatade414@gmail.com>
2020-01-12 17:48:20 -0500

    Heaps Sorted K lists
    Heaps Sorted K lists

---
+++ Leetcode-60/Heaps/SortedKList.java
@@ -0,0 +1,33 @@
+package Heaps;
+
+import java.util.PriorityQueue;
+
+public class SortedKList {
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	 }
+	
+	public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        for(ListNode head:lists){
+          while(head != null){    
+            minHeap.add(head.val);
+            head = head.next;
+          }  
+        }
+        
+        ListNode result = new ListNode(-1);
+        ListNode head = result;
+        
+        while(!minHeap.isEmpty()){
+            head.next = new ListNode(minHeap.remove());
+            head = head.next;
+        }
+        
+        return result.next; 
+        
+    }
+}
