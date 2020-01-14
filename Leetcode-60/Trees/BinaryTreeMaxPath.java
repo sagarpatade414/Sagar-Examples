0416bab20c16d0c8b6ffd3e5cfab17f42e912707
Sagar Patade <sagarpatade414@gmail.com>
2020-01-13 23:12:03 -0500

    BT MaxPath Edits
    BT MaxPath Edits

--- Leetcode-60/Trees/BinaryTreeMaxPath.java
+++ Leetcode-60/Trees/BinaryTreeMaxPath.java
@@ -20,7 +20,7 @@ public class BinaryTreeMaxPath {
         int right = Math.max(0,pathSum(root.right));
         
         maxSum = Math.max(maxSum, left + right + root.val);
-        return maxSum;
+        return Math.max(left,right)+root.val;
     }
     
     public class TreeNode {
