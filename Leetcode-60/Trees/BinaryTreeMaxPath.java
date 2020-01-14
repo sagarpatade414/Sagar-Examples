539ac5de951fea1ad50982d0378ee62fb7572041
Sagar Patade <sagarpatade414@gmail.com>
2020-01-13 23:09:27 -0500

    BT MaxPath
    BT MaxPath

---
+++ Leetcode-60/Trees/BinaryTreeMaxPath.java
@@ -0,0 +1,32 @@
+package Trees;
+
+public class BinaryTreeMaxPath {
+	
+	
+	int maxSum;
+    
+    public int maxPathSum(TreeNode root) {
+       maxSum = Integer.MIN_VALUE; 
+       pathSum(root);
+       return  maxSum;  
+    }
+    
+    private int pathSum(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        
+        int left = Math.max(0,pathSum(root.left));
+        int right = Math.max(0,pathSum(root.right));
+        
+        maxSum = Math.max(maxSum, left + right + root.val);
+        return maxSum;
+    }
+    
+    public class TreeNode {
+	     int val;
+	     TreeNode left;
+		 TreeNode right;
+		 TreeNode(int x) { val = x; }
+   }
+}
