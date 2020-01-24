5b4df54665c649692aeda9b9ca9fef02431bd0f5
Sagar Patade <sagarpatade414@gmail.com>
2020-01-23 20:13:40 -0500

    Rotate Image
    Rotate Image

---
+++ Leetcode-60/Matrix/RotateImage.java
@@ -0,0 +1,26 @@
+package Matrix;
+
+public class RotateImage {
+	public void rotate(int[][] matrix) {
+	       int N = matrix.length; 
+	       if(N == 0){
+	            return;
+	        }
+	        
+	        for(int i=0; i<N; i++){
+	            for(int j=i; j<N; j++){
+	                int temp = matrix[i][j];
+	                matrix[i][j] = matrix[j][i];
+	                matrix[j][i] = temp;
+	            }
+	        } 
+	        
+	        for(int i=0; i<N; i++){
+	            for(int j=0; j<(N/2); j++){
+	                int temp = matrix[i][j];
+	                matrix[i][j] = matrix[i][N-1-j];
+	                matrix[i][N-1-j] = temp;
+	            }
+	        }
+	    }
+}
