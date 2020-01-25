6147f4704651fc3418dfaff85855cf5122419d25
Sagar Patade <sagarpatade414@gmail.com>
2020-01-25 11:25:45 -0500

    Repeated Characters in a string
    Repeated Characters in a string

--- src/facebook/CountAsInString.java
+++ src/facebook/CountAsInString.java
@@ -2,8 +2,47 @@ package facebook;
 
 public class CountAsInString {
 	
+	// Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
 
-	
+        if(s.length() == 0){
+            return 0;
+        }
+
+        long whole = n/s.length();
+        int remainder = (int)(n%s.length());
+        int found = 0;
+        for(char ch : s.toCharArray()){
+            if(ch == 'a'){
+                found++;
+            }
+        }
+
+        long total = whole * found;
+        
+        if(remainder == 0){
+            return total;
+        } else {
+            
+            String remStr = s.substring(0, remainder);
+            for(char ch : remStr.toCharArray()){
+                if(ch == 'a'){
+                    total++;
+                }
+            }   
+              
+        }
+
+        return total;
+
+    }
+    
+    public static void main(String[] args){
+        String s = "aba";
+        long n = 10;
+        long result = repeatedString(s, n);
+        
+    }
 	
 	
 }
