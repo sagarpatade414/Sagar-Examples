2f1aea9c40407ed13c66511dba0564b46877f9d3
Sagar Patade <sagarpatade414@gmail.com>
2020-01-24 19:36:58 -0500

    Longest SubString explaination
    Longest SubString explaination

--- Leetcode-60/SlidingWindow/LongestSubstringWithoutRepeatingCharacters.java
+++ Leetcode-60/SlidingWindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -5,7 +5,13 @@ import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 	
-	public int lengthOfLongestSubstring(String s) {
+	public static void main(String[] args) {
+		 String s= "abcabcdbb";
+		 int stringLen = lengthOfLongestSubstring(s);
+		 System.out.println("stringLen : " + stringLen);
+	}
+	
+	public static int lengthOfLongestSubstring(String s) {
         int i=0;
         int j=0; 
         int max=0;
@@ -13,7 +19,8 @@ public class LongestSubstringWithoutRepeatingCharacters {
         Set<Character> hset = new HashSet<>();
         
         while(j<s.length()){
-            
+            System.out.println("charAt(j) :" +s.charAt(j));
+        	System.out.println(hset);
             if(!hset.contains(s.charAt(j))){
                 hset.add(s.charAt(j++));
                 max = Math.max(max, hset.size());
