eeead2a3aab1416095120fad9a3676d6d501c86e
Sagar Patade <sagarpatade414@gmail.com>
2020-01-27 08:50:27 -0500

    Common SubString
    Common SubString

--- src/facebook/CommonSubString12.java
+++ src/facebook/CommonSubString12.java
@@ -1,5 +1,37 @@
 package facebook;
 
-public class CommonSubString12 {
+import java.util.HashSet;
+import java.util.Set;
 
+public class CommonSubString12 {
+	
+	// Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+
+        if( s1.length() <= 0 || s2.length() <= 0) {
+            return "NO";
+        }
+
+        Set<Character> s1Ch = new HashSet<>();
+        Set<Character> s2Ch = new HashSet<>();
+        
+
+        for(char c :s1.toCharArray()){
+            s1Ch.add(c);
+        } 
+
+        for(char c :s2.toCharArray()){
+            s2Ch.add(c);
+        }
+
+        s1Ch.retainAll(s2Ch);
+
+        if(!s1Ch.isEmpty()){
+            return "YES";
+        } else { 
+            return "NO";
+        }
+        
+    }
+    
 }
