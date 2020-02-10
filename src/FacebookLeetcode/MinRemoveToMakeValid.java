3f19fec653a4d7281965fad8d69be38bcd8acce7
Sagar Patade <sagarpatade414@gmail.com>
2020-02-09 19:08:27 -0500

    Minimum remove to make valid string
    Minimum remove to make valid string

---
+++ src/FacebookLeetcode/MinRemoveToMakeValid.java
@@ -0,0 +1,42 @@
+package FacebookLeetcode;
+
+public class MinRemoveToMakeValid {
+	
+	public static String minRemoveToMakeValid(String s) {
+        
+        StringBuilder sb = new StringBuilder();
+        int open=0;
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                open++;
+            } else if(s.charAt(i) == ')'){
+                if(open == 0){
+                    continue;
+                }
+                open--;
+            }
+            
+           sb.append(s.charAt(i)); 
+        }
+        
+        StringBuilder result = new StringBuilder();
+        
+        for(int i=sb.length()-1; i>=0; i--){
+           if(sb.charAt(i) == '(' && open > 0) {
+              open--;
+              continue; 
+           }
+           result.append(sb.charAt(i)); 
+        }
+        
+       return  result.reverse().toString();
+    }
+	
+	public static void main(String[] main){
+    	String order = "lee(t(c)o)de)"; 
+    	String minRemoveToMakeValid = minRemoveToMakeValid(order);
+    	System.out.println("minRemoveToMakeValid : " + minRemoveToMakeValid);
+    }
+	
+}
