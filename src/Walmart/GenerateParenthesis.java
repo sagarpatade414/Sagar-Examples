2a87e8a6a69ff48f30bafeb894a8a1038595f615
Sagar Patade <sagarpatade414@gmail.com>
2020-03-21 22:45:26 -0400

    Generate Parenthesis
    Generate Parenthesis

---
+++ src/Walmart/GenerateParenthesis.java
@@ -0,0 +1,37 @@
+package Walmart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParenthesis {
+	
+	public static List<String> generateParenthesis(int n) {
+	       List<String> result = new ArrayList<>();
+	       helper(result, "", n, n);
+	       return result;
+	}
+	    
+	private static void helper(List<String> result, String curr, int left, int right){
+	        
+	        if(left<0 || left>right){
+	            return;
+	        }
+	        
+	        if(left==0 && right==0){
+	            result.add(curr);
+	            return;
+	        }
+	        
+	        helper(result, curr + "(", left-1, right);
+	        helper(result, curr + ")", left, right-1);
+	}
+	    
+	
+	public static void main(String[] args) {
+		
+		int n=3;
+		            		   
+		List<String> generateParenthesis = generateParenthesis(n);
+		System.out.println(generateParenthesis.toString());
+	}
+}
